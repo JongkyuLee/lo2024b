254e182e35c7ca6754b8d4f2a8bf361889fe50ad
JongkyuLee <44962800+JongkyuLee@users.noreply.github.com>
2025-08-07 10:21:39 +0900

    Update netlib_test.py

--- netlib_test.py
+++ netlib_test.py
@@ -1,5 +1,6 @@
 from functions.implementation import *
 import os
+import time
 
 directory = 'netlib_mat/'
 sorted_files = sorted(os.listdir(directory))
@@ -9,12 +10,17 @@ theta = 0.5
 int_mu = 1
 
 result = []
+result_time = []
+
 for filename in sorted_files:
     print("====================================")
+    print(filename)
     file_path = os.path.join(directory, filename)
     int_x, int_y, int_s, A, b, c, m, n = netlib(file_path, int_mu)
 
     tmp_result = []
+    tmp_time_result = []
+
     test_kernel_list = [2, 3, 4, 5, "mousaab", "benhadid21", "benhadid23", "fathi"]
     for ker_num in test_kernel_list:
         x, y, s = int_x.copy(), int_y.copy(), int_s.copy()
@@ -22,13 +28,23 @@ for filename in sorted_files:
 
         p = p_value(ker_num, n)
         q = q_value(ker_num, n, p)
+        start_time = time.time()
         total_itr = implementation(ker_num, A, p, q, m, n, mu, x, y, s, theta, epsilon, tau)
-        tmp_result.append(total_itr)
+        elapsed_time = time.time() - start_time
 
-    print(f"{filename} : {tmp_result}")
+        tmp_result.append(total_itr)
+        tmp_time_result.append(round(elapsed_time, 4))
+
+    print(f"{m} : {tmp_result}")
+    print(f"{m} : {tmp_time_result}")
+
     result.append(tmp_result)
+    result_time.append(tmp_time_result)
 
 result = np.array(result)
 print(result)
+
+print("Final Time Results (seconds):")
+print(result_time)
 
 
